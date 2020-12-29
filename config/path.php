565dceba5900e8f8a90d108a0de9d25d001709dd
BloodhunterD <bloodhunterd@bloodhunterd.com>
2020-12-29 21:05:31 +0100

    CHANGE #documentation #phpdoc #comment #copyright Comment for copyright adjusted by PhpDoc.

--- config/path.php
+++ config/path.php
@@ -1,12 +1,9 @@
 <?php
-/**
- * This file is part of the Backup project.
- * Visit project at https://github.com/bloodhunterd/backup
- *
- * Copyright © 2019 BloodhunterD <bloodhunterd@bloodhunterd.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+/*
+ * @package    Backup
+ * @author     BloodhunterD <bloodhunterd@bloodhunterd.com>
+ * @link       https://github.com/bloodhunterd
+ * @copyright  © 2020 BloodhunterD
  */
 
 declare(strict_types = 1);
