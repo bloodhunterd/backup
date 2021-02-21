4eabdf0fcfc1eab20d24988716dc5b87ec968251
BloodhunterD <bloodhunterd@bloodhunterd.com>
2021-02-21 21:38:53 +0100

    FIX #configuration #debug Fixed missing debug mode definition in config file.

--- src/Configuration.php
+++ src/Configuration.php
@@ -112,7 +112,7 @@ class Configuration
      */
     public function isDebugEnabled(): bool
     {
-        return $this->settings['debug'];
+        return $this->settings['debug'] ?? false;
     }
 
     /**
